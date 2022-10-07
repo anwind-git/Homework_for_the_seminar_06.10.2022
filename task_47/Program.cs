4afc4c954264068b967043650b813728e3008962
anwind-git <work-git@yandex.ru>
2022-10-07 16:07:33 +0300

    двумерный массив размером m×n, заполненный случайными вещественными числами, округлёнными до одного знака

---
+++ task_47/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами, 
+// округлёнными до одного знака.
+
+void Zadaha47()
+{
+    Random random = new Random();
+    int rows = random.Next(4, 8);
+    int colunms = random.Next(4, 8);
+    Console.WriteLine($"Массив размера {rows}x{colunms}");
+
+    double[,] numbers = new double[rows, colunms];
+
+    FillArray(numbers);
+    PrintArray(numbers);
+}
+
+void FillArray(double[,] numbers)
+{
+    Random random = new Random();
+    int rows = numbers.GetLength(0);
+    int colunms = numbers.GetLength(1);
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < colunms; j++)
+        {
+            numbers[i, j] = Math.Round(random.NextDouble() * 20 - 10, 1);
+        }
+    }
+}
+void PrintArray(double[,] numbers)
+{
+    int rows = numbers.GetLength(0);
+    int colunms = numbers.GetLength(1);
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < colunms; j++)
+        {
+            Console.Write(numbers[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Zadaha47();
